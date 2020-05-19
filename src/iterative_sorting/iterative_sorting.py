bf63f54d890c8f333776d077ccde1dcd2e917426
adastraz <tylergosox@gmail.com>
2020-05-19 11:51:58 -0400

    python is acting real weird today

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,18 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
         for x in range(cur_index, len(arr)):
             if arr[x] < arr[smallest_index]:
                 smallest_index = x
-        smallnum = arr[smallest_index]
-        arr.pop(smallest_index)
-        arr.insert(cur_index, smallnum)
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
- 
+    
 
     return arr
 
